b94a35bc470c9bb1e0e1e828fb7636cefc00cb8f
mtmcduffie <mcduffie.michael@gmail.com>
2019-01-14 11:38:47 -0500

    Formatting update.

--- cli/interface/cli.py
+++ cli/interface/cli.py
@@ -83,7 +83,7 @@ class ExitCmd(cmd.Cmd):
         Overrides default method to catch ctrl-c and exit gracefully.
         """
         print(self.intro)
-        if not user_prompt_yn("Do you agree to the above terms and conditions? "):
+        if not user_prompt_yn("Do you agree to the above terms and conditions? (Y/N)"):
             return True
         while True:
             try:
@@ -149,25 +149,33 @@ class CIDCCLI(ExitCmd, ShellCmd):
     intro = (
         "Welcome to the CIDC Command Line Interface (CLI) Tool."
         "\n"
+        "\n"
         "You are about to access a system which contains data protected by federal law."
+        "\n"
         "\n"
         "Unauthorized use of this system is strictly prohibited and subject to criminal "
         "and civil penalties. All information stored on this system is owned by the "
         "National Cancer Institute (NCI)."
         "\n"
+        "\n"
         "By using this tool, you consent to the monitoring and recording of your "
         "actions on this system. You also agree to refrain from engaging in any illegal "
         "or improper behavior while using this system."
+        "\n"
         "\n"
         "By downloading any data from the CIDC information system, you are agreeing to "
         "take responsibility for the security of said data. You may not copy, transmit, "
         "print out, or in any way cause the information to leave a secured computing "
         "environment where it may be seen or accessed by unauthorized individuals."
         "\n"
+        "\n"
         "Sharing your account with anyone else is strictly prohibited."
+        "\n"
         "\n"
         "If you become aware of any threat to the system or possible breach of data, "
         "you are required to immediately notify the CIDC."
+        "\n"
+        "\n"
     )
 
     def do_upload_data(self, rest=None) -> None:  # pylint: disable=W0613
@@ -210,7 +218,7 @@ class CIDCCLI(ExitCmd, ShellCmd):
         Returns:
             None -- [description]
         """
-        if not user_prompt_yn("Do you agree to the above terms and conditions?"):
+        if not user_prompt_yn("Do you agree to the above terms and conditions? (Y/N)"):
             return True
         return False
 
