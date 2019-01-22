ad2937284e704c0b57567b83ac30daa1bcd615fc
Lloyd McCarthy <lloydmccar@gmail.com>
2019-01-22 11:59:27 -0500

    Made changes to the readme to show branch coverage in table. Altered several text prints and procedures to reflect Ethan's requested changes. Removed code related to legacy upload options

--- cli/tests/test_interface.py
+++ cli/tests/test_interface.py
@@ -4,12 +4,17 @@ Tests for the command line interface
 """
 from typing import Callable, List
 from unittest.mock import patch
-from unittest import TestCase
 
-from cidc_utils.caching import CredentialCache
-
-import utilities.cli_utilities
-from utilities.cli_utilities import *
+from utilities.cli_utilities import (
+    generate_options_list,
+    ensure_logged_in,
+    option_select_framework,
+    cache_token,
+    force_valid_menu_selection,
+    user_prompt_yn,
+    get_valid_dir,
+)
+from constants import USER_CACHE
 
 
 def mock_with_inputs(inputs: List[object], function: Callable, arguments: List[object]):
@@ -93,103 +98,14 @@ def test_ensure_logged_in():
     """
     Test function for ensure_logged_in
     """
-    cache = CredentialCache(100, 300)
-    cache.cache_key("foo")
-    utilities.cli_utilities.USER_CACHE = cache
+
+    USER_CACHE.cache_key("foo")
     assert ensure_logged_in()
-
-
-def test_get_files():
-    """
-    Test for get_files,
-    """
-    with patch(
-        "utilities.cli_utilities.get_valid_dir",
-        return_value=("./some/path", ["file_3D", "file2_3D"]),
-    ):
-        with patch(
-            "utilities.cli_utilities.validate_and_extract",
-            return_value={"foo": {"sample_id": "3D", "mapping": "file2"}},
-        ):
-            assert get_files(["3D"], ["file", "file2"]) == (
-                {"foo": {"sample_id": "3D", "mapping": "file2"}},
-                "./some/path",
-            )
-
-
-def test_create_payload_objects():
-    """
-    Test for create_payload_objects
-    """
-
-    file_dict = {"file_name_1": {"mapping": "map", "sample_id": "1234"}}
-    assay = {"assay_id": "321"}
-    trial = {"_id": "456"}
-    payload = create_payload_objects(file_dict, trial, assay)
-    assert payload == [
-        {
-            "assay": "321",
-            "trial": "456",
-            "file_name": "file_name_1",
-            "sample_id": "1234",
-            "mapping": "map",
-        }
-    ]
-
-
-def test_validate_and_extract():
-    """
-    Test function for validate_and_extract
-    """
-    # Test good input.
-    inputs = ["file1", "file2"]
-    arguments_good = [
-        ["file1_3D", "file2_3D", "file1_4C", "file2_4C"],
-        ["3D", "4C"],
-        inputs,
-    ]
-    result = mock_with_inputs(
-        ["1", "1", "1", "1"], validate_and_extract, arguments_good
-    )
-    assert result == {
-        "file1_3D": {"sample_id": "3D", "mapping": "file1"},
-        "file2_3D": {"sample_id": "3D", "mapping": "file2"},
-        "file1_4C": {"sample_id": "4C", "mapping": "file1"},
-        "file2_4C": {"sample_id": "4C", "mapping": "file2"},
-    }
-    # Test unmatched sample ID.
-    arguments_bad = [["file1_4B", "file2_3D"], ["3D"], inputs]
-    result_bad = mock_with_inputs(["1", "1"], validate_and_extract, arguments_bad)
-    assert not result_bad
-
-
-class TestValidateThrow(TestCase):
-    """
-    Simple test to confirm correct error is raised.
-
-    Arguments:
-        TestCase {unittest.TestCase} -- unittest testcase class.
-    """
-    def test_validate_and_extract_error(self):
-        """
-        Assert calling too many files raises error.
-        """
-        inputs = ["file1", "file2"]
-        arguments_many = [["file1_3D_B", "file1_3D", "file2_3D"], ["3D"], inputs]
-        self.assertRaises(
-            RuntimeError,
-            mock_with_inputs,
-            ["1", "1"],
-            validate_and_extract,
-            arguments_many,
-        )
 
 
 def test_store_token():
     """
     Test for storing/retrieving a token in Cache.
     """
-    from utilities.cli_utilities import USER_CACHE
-
     cache_token("test")
-    assert USER_CACHE.get_key() == "test"
+    assert USER_CACHE.get_key() is "test"
--- cli/utilities/cli_utilities.py
+++ cli/utilities/cli_utilities.py
@@ -3,14 +3,10 @@ Utility methods for the CIDC-CLI Interface
 """
 # pylint: disable=R0903
 import os
-import re
 from typing import List, Tuple, NamedTuple
 from cidc_utils.requests import SmartFetch
-from cidc_utils.caching import CredentialCache
-from auth0.auth0 import run_auth_proc
-from constants import EVE_URL
+from constants import EVE_URL, USER_CACHE
 
-USER_CACHE = CredentialCache(100, 600)
 EVE_FETCHER = SmartFetch(EVE_URL)
 
 
@@ -22,6 +18,32 @@ class Selections(NamedTuple):
     eve_token: str
     selected_trial: dict
     selected_assay: dict
+
+
+def terminal_sensitive_print(message: str, width: int = 80) -> None:
+    """
+    Prints a given string with a number of characters as a max width. Attempts to respect
+    whitespaces.
+
+    Arguments:
+        message {str} -- Message to be printed
+        width {int} -- Terminal width
+
+    Returns:
+        None -- [description]
+    """
+    for _ in range(0, len(message), width):
+        blank = False
+        chars = width + 1
+        while not blank:
+            chars -= 1
+            if message[_ : _ + chars] == " ":
+                blank = True
+            if chars <= 60:
+                blank = True
+                chars = width
+
+        print(message[_ : _ + chars].strip())
 
 
 def generate_options_list(options: List[str], header: str) -> str:
@@ -146,23 +168,20 @@ def option_select_framework(options: List[str], prompt_header: str) -> int:
 
 def ensure_logged_in() -> str:
     """
-    Checks if the user is logged in, and if they are not, promps them to log in.
+    Checks if the user is logged in, and if they are not, prompts them to log in.
 
     Returns:
         str -- API access token
     """
-    creds = USER_CACHE.get_key()
-    if not creds:
-        print(
-            "You are not currently authenticated. Launching a page to sign in with google"
-        )
-        creds = run_auth_proc()
-        if not creds:
-            print("Authentication failed!")
-            return None
-        USER_CACHE.cache_key(creds)
 
-    return creds
+    if not USER_CACHE.get_key():
+        print(
+            "You are not currently authenticated. Run 'jwt_login' to log in\b"
+            + "with a token. If you do not have a token, you can get one from the website."
+        )
+        return None
+
+    return USER_CACHE.get_key()
 
 
 def cache_token(token: str) -> None:
@@ -176,6 +195,30 @@ def cache_token(token: str) -> None:
         None -- [description]
     """
     USER_CACHE.cache_key(token)
+
+
+def run_jwt_login(token: str) -> None:
+    """
+    Takes a user's JWT and confirms its validity.
+    If it is valid
+
+    Arguments:
+        token {str} -- JWT
+
+    Returns:
+        None -- [description]
+    """
+    if not token:
+        print("Please enter a token when running this command")
+        return
+    try:
+        EVE_FETCHER.get(token=token, endpoint="trials")
+        cache_token(token)
+        print("Token is valid, you are now logged in!")
+    except RuntimeError:
+        print(
+            "Your token is invalid. Please make sure your token was entered correctly"
+        )
 
 
 def user_prompt_yn(prompt: str) -> bool:
@@ -196,57 +239,6 @@ def user_prompt_yn(prompt: str) -> bool:
     if selection in {"y", "yes", "Y", "Yes", "YES"}:
         return True
     return False
-
-
-def get_files(sample_ids: List[str], non_static_inputs: List[str]) -> Tuple[dict, str]:
-    """
-    Asks for user to input a directory, then fetches all files from it.
-
-    Arguments:
-        sample_ids {[str]} -- List of sample IDs from selected trial.
-        non_static_inputs {[str]} -- Variable inputs from selected assay.
-
-    Returns:
-        Tuple[dict, str] -- Upload dictionary, and upload directory.
-    """
-    valid_sample_ids = False
-
-    while not valid_sample_ids:
-        upload_dir, files_to_upload = get_valid_dir(is_download=False)
-        upload_dictionary = validate_and_extract(
-            files_to_upload, sample_ids, non_static_inputs
-        )
-        if upload_dictionary:
-            return upload_dictionary, upload_dir
-
-        print("Files contained invalid IDs!")
-
-
-def create_payload_objects(
-    file_dict: List[dict], trial: dict, assay: dict
-) -> List[dict]:
-    """
-    Returns objects formatted for inserting into the API
-
-    Arguments:
-        file_dict {List[dict]} -- List of file objects.
-        trial {dict} -- Trial ID dictionary
-        assay {dict} -- Assay ID dictionary
-
-    Returns:
-        List[dict] -- List of records in payload format.
-    """
-    return [
-        {
-            "assay": assay["assay_id"],
-            "experimental_strategy": assay["assay_name"],
-            "trial": trial["_id"],
-            "file_name": key,
-            "sample_ids": [file_dict[key]["sample_id"]],
-            "mapping": file_dict[key]["mapping"],
-        }
-        for key in file_dict
-    ]
 
 
 def select_assay_trial(prompt: str) -> Selections:
@@ -272,8 +264,10 @@ def select_assay_trial(prompt: str) -> Selections:
         response = EVE_FETCHER.get(token=eve_token, endpoint="trials")
     except RuntimeError as rte:
         if "401" in str(rte):
-            print("Error: You have not yet registered on our portal website!")
-            print("Please go to our website to register.")
+            print(
+                "Error: Your credentials could not be validated. Please check that you have "
+                + "registered on our website, and that you correctly copied the token."
+            )
         else:
             print("ERROR: %s" % rte)
 
@@ -281,7 +275,7 @@ def select_assay_trial(prompt: str) -> Selections:
     trials = response.json()["_items"]
 
     if not trials:
-        print("No trials were found for your id, your credentials will not be saved")
+        print("No trials were found for this user.")
         return None
 
     selected_trial = trials[
@@ -303,79 +297,3 @@ def select_assay_trial(prompt: str) -> Selections:
     return Selections(
         eve_token, selected_trial, selected_trial["assays"][assay_selection - 1]
     )
-
-
-def validate_and_extract(
-    file_names: List[str], sample_ids: List[str], non_static_inputs: List[str]
-) -> dict:
-    """
-    Compares each file name in upload to list of valid sample IDs in trial.
-    If all names are valid, returns a mapping of filename to sample id.
-
-    Arguments:
-        file_names {List[str]} -- list of file names
-        sample_ids {List[str]} -- list of valid ids
-        non_static_inputs {List[str]} -- list of non static inputs for selected assay.
-    Returns:
-        dict -- Dictionary mapping file name to sample id. Format:
-            {
-                file_name: {
-                    sample_id: "sample_id",
-                    mapping: "mapping"
-                }
-            }
-    """
-    # Create RE object based on valid samples
-    search_string = re.compile(str.join("|", sample_ids))
-    # create a dictionary of type filename: regex search result
-    name_dictionary = dict(
-        (item, re.search(search_string, item)) for item in file_names
-    )
-    # Check for any "None" types and return empty list if any found.
-    if not all(name_dictionary.values()):
-        return []
-    # If all valid, return map of filename: sample_id
-    mapped_names = dict(
-        (name, name_dictionary[name].group()) for name in name_dictionary
-    )
-    sample_id_dict = {}
-
-    # Group filenames under associated sampleIDs.
-    # Format: sample_id: [list, of, files, with, that, id]
-    for key, value in mapped_names.items():
-        if value in sample_id_dict:
-            sample_id_dict[value].append(key)
-        else:
-            sample_id_dict[value] = [key]
-
-    # Guide for associating file names with sample ids, and later, inputs
-    upload_guide = dict(
-        (item, {"sample_id": mapped_names[item]}) for item in mapped_names
-    )
-
-    for sample_id in sample_id_dict:
-
-        # Copy list by value for manipulation
-        nsi = non_static_inputs[:]
-
-        print(
-            "These files are associated with SampleID: %s, please map them to the assay inputs"
-            % sample_id
-        )
-        files_to_map = sample_id_dict[sample_id]
-
-        # Sanity check number of files per ID
-        if len(files_to_map) > len(nsi):
-            print("Error! Too many files for this sampleID")
-            raise RuntimeError
-
-        # Loop over files and make the user map them.
-        for filename in files_to_map:
-            selection = option_select_framework(
-                nsi, "Please choose which input %s maps to." % filename
-            )
-            # save selection, then delete from list.
-            upload_guide[filename]["mapping"] = nsi[selection - 1]
-            del nsi[selection - 1]
-
-    return upload_guide
