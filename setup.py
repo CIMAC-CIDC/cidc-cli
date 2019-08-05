8d45e9b1791bdcb2224fe6949bad99adaa1799cd
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-08-05 10:52:49 -0400

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ Setup for the package
 from setuptools import setup, find_packages
 
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="cidc_cli",
     version="0.1.0",
@@ -16,5 +19,6 @@ setup(
             "cidc = cli2.cli:cidc",
         ]
     },
+    install_requires=requirements,
     python_requires=">=3.6",
 )
