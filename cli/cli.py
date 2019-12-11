148e5453627230e7d8ea5b73e510141cc2350199
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-12-11 13:50:42 -0500

    Add the `analysis` command group and upload logic (#105)
    * Add analysis command group and upload logic
    
    * Remove 'x'
    
    * Fix test typo
    
    * Bump package version
    
    * Make env warning global

--- cli/cli.py
+++ cli/cli.py
@@ -5,8 +5,10 @@ from . import api, auth, gcloud, upload, config, consent
 
 #### $ cidc ####
 @click.group()
-def cidc():
+@click.option("-i", "--ignore", default=None, hidden=True)
+def cidc(ignore):
     """The CIDC command-line interface."""
+    config.check_env_warning(ignore)
     if not consent.check_consent():
         exit(0)
     gcloud.check_installed()
@@ -48,10 +50,8 @@ def get_env():
 
 #### $ cidc assays ####
 @click.group()
-@click.option("-i", "--ignore", default=None, hidden=True)
-def assays(ignore):
+def assays():
     """Manage assay data."""
-    config.check_env_warning(ignore)
 
 
 #### $ cidc assays list ####
@@ -71,12 +71,41 @@ def upload_assay(assay, xlsx):
     """
     Upload data for an assay.
     """
-    upload.upload_assay(assay, xlsx)
+    upload.run_upload(assay, xlsx)
+
+
+#### $ cidc analyses ####
+@click.group()
+def analyses():
+    """Manage analysis data."""
+
+
+#### $ cidc analyses list ####
+@click.command("list")
+def list_analyses():
+    """List all supported analysis types."""
+    analysis_list = api.list_analyses()
+    for analysis in analysis_list:
+        click.echo(f"* {analysis}")
+
+
+#### $ cidc analyses upload ####
+@click.command("upload")
+@click.option("--analysis", required=True, help="Analysis type.")
+@click.option(
+    "--xlsx", required=True, help="Path to the analysis metadata spreadsheet."
+)
+def upload_analysis(analysis, xlsx):
+    """
+    Upload data for an analysis.
+    """
+    upload.run_upload(analysis, xlsx, is_analysis=True)
 
 
 # Wire up the interface
 cidc.add_command(login)
 cidc.add_command(assays)
+cidc.add_command(analyses)
 cidc.add_command(config_)
 
 config_.add_command(set_env)
@@ -84,6 +113,9 @@ config_.add_command(get_env)
 
 assays.add_command(list_assays)
 assays.add_command(upload_assay)
+
+analyses.add_command(list_analyses)
+analyses.add_command(upload_analysis)
 
 if __name__ == "__main__":
     cidc()
