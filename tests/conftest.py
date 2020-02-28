bf62ae14f4f981684273383ca88119355c76518d
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-02-28 14:25:19 -0500

    Create a testing directory if one doesn't exist

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,14 @@ from os.path import abspath, dirname
 import pytest
 from click.testing import CliRunner
 
+from cli.config import CIDC_WORKING_DIR
+
 PACKAGE_PATH = abspath(dirname(dirname(__file__)))
 sys.path.insert(0, PACKAGE_PATH)
 
 os.environ["TESTING"] = "True"
+if not os.path.isdir(CIDC_WORKING_DIR):
+    os.mkdir(CIDC_WORKING_DIR)
 
 
 @pytest.fixture
